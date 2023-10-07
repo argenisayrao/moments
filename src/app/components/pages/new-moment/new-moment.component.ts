d010d166ef4e436f312819ef9ea16bf616deef53
argenisayrao <argenisayrao@gmail.com>
2023-10-07 16:35:48 -0300

    metade aula 29

--- src/app/components/pages/new-moment/new-moment.component.ts
+++ src/app/components/pages/new-moment/new-moment.component.ts
@@ -1,16 +1,35 @@
 import { Component, OnInit } from '@angular/core';
+import { Moment } from '../../Moment';
+import { MomentService } from 'src/app/services/moment.service';
 
 @Component({
   selector: 'app-new-moment',
   templateUrl: './new-moment.component.html',
-  styleUrls: ['./new-moment.component.css']
+  styleUrls: ['./new-moment.component.css'],
 })
 export class NewMomentComponent implements OnInit {
   btnText: string = 'Compartilhar!';
-  
-  constructor() { }
 
-  ngOnInit(): void {
+  constructor(private momentService: MomentService) {}
+
+  ngOnInit(): void {}
+
+  async createHandler(moment: Moment) {
+    const formData = new FormData();
+
+    formData.append('title', moment.title);
+    formData.append('description', moment.description);
+
+    if (moment.image) {
+      formData.append('image', moment.image);
+    }
+
+    //todo
+
+   await this.momentService.createMoment(formData).subscribe();
+
+    // exibir msg
+
+    // redirect
   }
-
 }
--- src/app/services/messages.service.ts
+++ src/app/services/messages.service.ts
@@ -3,7 +3,7 @@ import { Injectable } from '@angular/core';
 @Injectable({
   providedIn: 'root'
 })
-export class MessagesServicesService {
+export class MessagesService {
 
   constructor() { }
 }
