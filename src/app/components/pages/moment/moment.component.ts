9cdf42c932ed05466b0466e5dc22052c232e8a57
argenisayrao <argenisayrao@gmail.com>
2023-10-11 23:00:18 -0300

    fim aula 36

--- src/app/components/pages/moment/moment.component.ts
+++ src/app/components/pages/moment/moment.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { MomentService } from 'src/app/services/moment.service';
 import { Router, ActivatedRoute } from '@angular/router';
 import { Moment } from '../../Moment';
+import { environment } from 'src/environments/environment';
+import { faTimes, faEdit } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-moment',
@@ -10,13 +12,21 @@ import { Moment } from '../../Moment';
 })
 export class MomentComponent implements OnInit {
   moment?: Moment;
+  baseApiUrl = environment.baseApiUrl;
 
-  constructor(private momentService: MomentService,
-              private route: ActivatedRoute) {}
+  faTimes = faTimes;
+  faEdit = faEdit;
+
+  constructor(
+    private momentService: MomentService,
+    private route: ActivatedRoute
+  ) {}
 
   ngOnInit(): void {
-    const id = Number(this.route.snapshot.paramMap.get("id"))
+    const id = Number(this.route.snapshot.paramMap.get('id'));
 
-    this.momentService.getMoment(id).subscribe((item => this.moment = item.data))
+    this.momentService
+      .getMoment(id)
+      .subscribe((item) => (this.moment = item.data));
   }
 }
